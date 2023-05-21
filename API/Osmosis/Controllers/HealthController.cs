989bb7679a08472231b3d92b3162f4e1af5b6da7
MatheusZacarias657 <74462682+MatheusZacarias657@users.noreply.github.com>
2023-05-21 19:41:05 -0300

    Create HealthController.cs

---
+++ API/Osmosis/Controllers/HealthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Osmosis.Controllers.Base;
+using Osmosis.DAO;
+using Osmosis.DAO.Base;
+using Osmosis.Models;
+using Osmosis.Services;
+
+namespace Osmosis.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult IsAlive()
+        {
+            try
+            {
+                return Ok(new { alive = true });
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
